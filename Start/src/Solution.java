d77de40d9b1e36acd8860564c5a2a0b8eddef0a6
mae2312 <96482378+mae2312@users.noreply.github.com>
2022-08-08 23:32:59 +0600

    Start
    Start

---
+++ Start/src/Solution.java
@@ -0,0 +1,8 @@
+
+public class Solution {
+	public static void main(String[] args) {
+		System.out.print("I think ");
+		System.out.print("being a programmer ");
+		System.out.print("is cool.");
+	}
+}
